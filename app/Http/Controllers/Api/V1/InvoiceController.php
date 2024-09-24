9eb63da763b0d99a99c31e97d855256f839d0e5d
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-09-24 18:54:58 +1000

    complete how to build a REST API with laravel: PHP full course

---
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Filters\V1\InvoicesFilter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\BulkStoreInvoiceRequest;
+use App\Http\Requests\V1\StoreInvoiceRequest;
+use App\Http\Requests\V1\UpdateInvoiceRequest;
+use App\Http\Resources\v1\InvoiceCollection;
+use App\Http\Resources\v1\InvoiceResource;
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $filter = new InvoicesFilter();
+        $queryItems = $filter->transform($request); // [['column', 'operator', 'value']]
+
+        if (count($queryItems) === 0) {
+            return new InvoiceCollection(Invoice::paginate());
+        } else {
+            $invoices = new InvoiceCollection(Invoice::where($queryItems)->paginate());
+
+            return new InvoiceCollection($invoices->appends($request->query()));
+
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvoiceRequest $request)
+    {
+        //
+    }
+
+    public function bulkStore(BulkStoreInvoiceRequest $request)
+    {
+        $bulk = collect($request->all())->map(function($arr, $key) {
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+        });
+
+        Invoice::insert($bulk->toArray());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Invoice $invoice)
+    {
+        return new InvoiceResource($invoice);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInvoiceRequest $request, Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Invoice $invoice)
+    {
+        //
+    }
+}
---
+++ app/Models/Invoice.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'customer_id',
+      'amount',
+      'status',
+      'billed_date',
+      'paid_date',
+    ];
+
+    public function Customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
